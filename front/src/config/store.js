f7abd1d09da886759f01daa704970f7af77775bb
laique santana <laiquesantana@hotmail.com>
2020-11-08 01:43:09 -0300

    ciado um arquivo store js para atraves do vuex controlar o estado do toogle visible ou não

---
+++ front/src/config/store.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        isMenuVisible: true,
+        user:{
+            name: 'Monza',
+            email: 'Groupa_hires_me@grupoa.com'
+        }
+     
+    },
+    mutations: {
+        toggleMenu(state, isVisible) {
+     
+            if(isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+            
+        }
+ 
+    }
+})
