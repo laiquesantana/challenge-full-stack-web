a9babd5701b831fb7418a6958844bb3b3fb5bf47
laique santana <laiquesantana@hotmail.com>
2020-11-10 18:31:28 -0300

    dump do mysql com as tabelas de alunos, password e users para futuro login na API com JWT

---
+++ db/database.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS `passwordtokens`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `students`;
+
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT '0',
+  `email` varchar(150) NOT NULL DEFAULT '0',
+  `password` varchar(200) NOT NULL DEFAULT '0',
+  `role` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `passwordtokens` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `token` varchar(200) NOT NULL DEFAULT '0',
+  `user_id` int(10) unsigned NOT NULL DEFAULT 0,
+  `used` tinyint(3) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `FK_passwordtokens_users` (`user_id`),
+  CONSTRAINT `FK_passwordtokens_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT '0',
+  `academic_record` varchar(50) DEFAULT '0',
+  `email` varchar(150) NOT NULL DEFAULT '0',
+  `cpf` varchar(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+  UNIQUE KEY `academic_record` (`academic_record`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
